d659bc8c4cd6637bf4364fee19bc41fd97abbe80
daesung <daesung@mediaweb.co.kr>
2024-09-06 14:50:28 +0900

    Lottie_test

--- LottieLib_test/app/src/main/java/com/daeseong/lottielib_test/Main8Activity.java
+++ LottieLib_test/app/src/main/java/com/daeseong/lottielib_test/Main8Activity.java
@@ -1,7 +1,6 @@
 package com.daeseong.lottielib_test;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
